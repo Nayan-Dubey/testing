446de16e7dca3ec62597afc2335419794f02c8e3
Nayan Dubey <itsnayandubey@gmail.com>
2021-02-19 11:59:23 +0530

    main have been changed

--- main.cpp
+++ main.cpp
@@ -6,22 +6,9 @@ using namespace std;
 
 int main()
 {
-int t;
-cin>>t;
-while(t--)
-{
-    int l,r;
-    cin>>l>>r;
-    int c=0;
-    for(int i=l;i<=r;i++)
-    {
-        if(i%10==2||i%10==3||i%10==9)
-        {
-            c++;
-        }
-    }
-    cout<<c<<endl;
-}
+string s;
+cin>>s;
+cout<<s;
 
 return 0;
 }
